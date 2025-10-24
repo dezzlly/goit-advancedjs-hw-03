97be6f90c9a8a5c02eab2869d05f13897f22eff3
Alex Osypenko <alex.o@whitetech.com>
2025-10-24 10:42:42 +0400

    hw3

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -2,32 +2,17 @@ const API_KEY = '52905819-433962418f10cc5e4a65a61ca';
 const BASE_URL = 'https://pixabay.com/api/';
 
 
-function buildUrl(query) {
-const params = new URLSearchParams({
+export function fetchImages(query) {
+  const params = {
     key: API_KEY,
     q: query,
     image_type: 'photo',
     orientation: 'horizontal',
-    safesearch: 'true',
-    per_page: '40'
-    });
-    return `${BASE_URL}?${params.toString()}`;
-}
+    safesearch: true,
+    per_page: 40,
+  };
 
-
-
-export function fetchImages(query) {
-    const url = buildUrl(query);
-
-
-    return fetch(url)
-        .then(response => {
-            if (!response.ok) {
-                throw new Error(`HTTP ${response.status}`);
-            }
-            return response.json();
-        })
-        .catch(err => {
-            throw err;
-        });
+  return axios
+    .get(BASE_URL, { params })
+    .then(res => res.data);
 }
